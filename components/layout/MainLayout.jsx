cadf07d790894d70d28dd2b8f60b64ab4cc2835e
caraje <carlosajenjo@gmail.com>
2022-08-02 20:44:39 +0200

    Arreglos OpenGraph

--- components/layout/MainLayout.jsx
+++ components/layout/MainLayout.jsx
@@ -6,13 +6,12 @@ const MainLayout = ({children, title, url, img, description, author, tags}) => {
     return (
         <>
             <Head>
-                <title>{title}</title>
-                <meta name='og:url' content={url} />
-                <meta name='og:title' content={title} />
-                <meta name='og:image' content={img} />
-                <meta name='og:description' content={description} />
-                <meta name="twitter:card" content="summary" />
-                <meta name='twitter:creator' content={author} />
+                {/* <meta name='og:url' content={url} /> */}
+                {/* <meta name='og:title' content={title} /> */}
+                {/* <meta name='og:image' content={img} /> */}
+                {/* <meta name='og:description' content={description} /> */}
+                {/* <meta name="twitter:card" content="summary" /> */}
+                {/* <meta name='twitter:creator' content={author} /> */}
 
 
 
