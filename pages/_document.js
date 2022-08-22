dd3b01f07d3b2afd887cc8dfc62a81b26335c3e1
caraje <carlosajenjo@gmail.com>
2022-08-22 20:33:41 +0200

    estadisticas

--- pages/_document.js
+++ pages/_document.js
@@ -10,21 +10,21 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html lang="es">
+      <Html lang='es'>
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel='preconnect' href='https://fonts.googleapis.com' />
+          <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin />
+          <link rel='preconnect' href='https://fonts.googleapis.com' />
           <link
-            href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,800;0,900;1,300&display=swap"
-            rel="stylesheet"
+            href='https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,800;0,900;1,300&display=swap'
+            rel='stylesheet'
           />
           <script
-            src="https://kit.fontawesome.com/50b2cf911e.js"
-            crossOrigin="anonymous"
+            src='https://kit.fontawesome.com/50b2cf911e.js'
+            crossOrigin='anonymous'
             async
           ></script>
-          <link rel="shortcut icon" href="/favicon.ico" />
+          <link rel='shortcut icon' href='/favicon.ico' />
           <script
             async
             src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`}
@@ -80,6 +80,40 @@ class MyDocument extends Document {
                             `,
             }}
           />
+
+          {/* ____ */}
+
+          {/* <!-- Default Statcounter code for Dados y Pixeles https://www.dadosypixeles.es/
+--> */}
+          <script type='text/javascript'>
+            var sc_project=12786839; var sc_invisible=1; var
+            sc_security="b51cb029";
+          </script>
+          <script
+            type='text/javascript'
+            src='https://www.statcounter.com/counter/counter.js'
+            async
+          ></script>
+          <noscript>
+            <div class='statcounter'>
+              <a
+                title='Web Analytics'
+                href='https://statcounter.com/'
+                target='_blank'
+              >
+                <img
+                  class='statcounter'
+                  src='https://c.statcounter.com/12786839/0/b51cb029/1/'
+                  alt='Web Analytics'
+                  referrerPolicy='no-referrer-when-downgrade'
+                />
+              </a>
+            </div>
+          </noscript>
+          {/* <!-- End of Statcounter Code --> */}
+          <a href='https://statcounter.com/p12786839/?guest=1'>View My Stats</a>
+
+          {/* ____ */}
         </Head>
         <body>
           <Main />
@@ -89,7 +123,7 @@ class MyDocument extends Document {
           <FooterPage />
         </footer>
       </Html>
-    );
+    )
   }
 }
 
